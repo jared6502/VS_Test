dcbcee249fbd64c5b0193af4dc518142071ecaaf
jared6502 <jared6502@gmail.com>
2016-05-31 16:38:20 -0400

    changed button code

--- GitTest/Form1.vb
+++ GitTest/Form1.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Button clicked.")
+        Dim i As Integer
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim test As Integer = 42
+        MessageBox.Show(test.ToString)
     End Sub
 End Class
